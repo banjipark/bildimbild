948482f1dd73a369a15bd48523c329693bf22d52
Beni Arnold <banjipark@gmail.com>
2014-03-12 22:53:50 +0100

    used list comprehension to generate the pixel map.

--- ResizeImage.hs
+++ ResizeImage.hs
@@ -3,12 +3,14 @@ module ResizeImage where
 type Point = (Int,Int)
 type Resolution = (Int,Int)
 
-generatePixelMap :: Resolution -> Resolution -> [Point]
-generatePixelMap (srcX,srcY) (dstX,dstY) = projectPixel xFactor yFactor (5,3)
+generatePixelMap :: Resolution -> Resolution -> [[(Int, Int)]]
+generatePixelMap (srcX,srcY) (dstX,dstY) = [ projectPixel xFactor yFactor (x, y) | x <- [1..dstX], y <- [1..dstY] ]
                                         where
-                                            xFactor = dstX / srcX
-                                            yFactor = dstY / srcY
+                                            xFactor = fromIntegral srcX / fromIntegral dstX
+                                            yFactor = fromIntegral srcY / fromIntegral dstY
 
-projectPixel :: Num -> Num -> Point -> [Point]
-projectPixel c u (x,y) = [(2*x,2*y)]
+projectPixel :: (RealFrac a) => a -> a -> (Int, Int) -> [(Int, Int)]
+projectPixel c u (x,y) = [(round (c * (fromIntegral x)), round (u * (fromIntegral y)))]
 
+allPairs :: [(Int, Int)]
+allPairs = [ (x, y) | x <- [1..4], y <- [5..8] ]
